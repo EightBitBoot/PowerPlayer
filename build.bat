371756c1aeca6cff1c729cc7f270b899fb7dfc30
Adin W-T <adinrwt@gmail.com>
2021-04-27 12:36:14 -0400

    Added basic system tray icon with no way to exit

---
+++ build.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+cl main.cpp user32.lib shell32.lib
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
+#include <string.h>
+
 #include <windows.h>
+
+#define ICON_ID 1
+#define WM_ICON_MESSAGE (WM_USER)
 
 #define WINDOW_CLASS_NAME "PowerPlayerDummy"
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
     WNDCLASSA windowClass = {0};
@@ -14,20 +17,33 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
     int result = RegisterClassA(&windowClass);
     if(!result) {
-        MessageBoxA(NULL, "Ruh Roe Raggy", "Railed ro register rindow rass", MB_ICONERROR | MB_OK);
-        return -1;
+        MessageBoxA(NULL, "Failed to register window class!\nGo yell this at the developer.", "PowerPlayer: Error", MB_ICONERROR | MB_OK);
+        return 1;
     }
 
-    HWND dummyWindow = CreateWindowA(WINDOW_CLASS_NAME, "Nothing To See Here Officer", WS_DISABLED, 0, 0, 0, 0, NULL, NULL, hInstance, 0);
+    HWND dummyWindow = CreateWindowA(WINDOW_CLASS_NAME, "DummyWindow", WS_DISABLED, 0, 0, 0, 0, NULL, NULL, hInstance, 0);
     if(!dummyWindow) {
-        MessageBoxA(NULL, "Oops I Did It Again", "Window is no no", MB_ICONERROR | MB_OK);
-        return -1;
+        MessageBoxA(NULL, "Failed to create dummy window!\nGo yell this at the developer.", "PowerPlayer: Error", MB_ICONERROR | MB_OK);
+        return 1;
     }
 
-    NOTIFYICONDATA notifIcon = {0};
-    notifIcon.cbSize = sizeof(NOTIFYICONDATA);
-    notifIcon.hWnd = dummyWindow;
+    char *szTip = "PowerPlayer";
+    HICON hIcon = (HICON) LoadImageA(NULL, "PowerPlayer.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE);
+    if(!hIcon) {
+        MessageBoxA(NULL, "Failed to load icon!", "PowerPlayer: Error", MB_ICONERROR | MB_OK);
+        return 1;
+    }
 
+    NOTIFYICONDATAA iconData = {0};
+    iconData.cbSize = sizeof(iconData);
+    iconData.hWnd = dummyWindow;
+    iconData.uID = ICON_ID;
+    iconData.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
+    iconData.uCallbackMessage = WM_ICON_MESSAGE;
+    iconData.hIcon = hIcon;
+    memcpy(iconData.szTip, szTip, strlen(szTip));
+
+    Shell_NotifyIconA(NIM_ADD, &iconData);
 
     MSG msg = {0};
     while(GetMessage(&msg, NULL, 0, 0)) {
@@ -35,5 +51,14 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
         DispatchMessage(&msg);
     }
 
+    Shell_NotifyIconA(NIM_DELETE, &iconData);
+
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch(uMsg) {
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
 }
